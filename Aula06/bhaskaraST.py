392ee82857ad56eee860d1f679ef04c29d102103
gucruz9 <contato.one9@gmail.com>
2025-11-05 21:38:45 -0300

    bhaskaraST.py

--- Aula06/bhaskaraST.py
+++ Aula06/bhaskaraST.py
@@ -1,33 +1,41 @@
-import streamlit as st
-import math 
+from streamlit import header,write,text_input,text_input,button,warning,success,error
+from math import sqrt, pow
 
-st.header ('Calculadora de Bhaskara')
-st.write ("Calculadora de raízes \n de uma equação de segundo grau")
-st.write ( "ax² + bx + c = 0" )
+#Função python
+def calculo(deltaCLodoaldo):
+    valor = (sqrt(deltaCLodoaldo)) / (2*a)
+    return valor 
+
+
+header ('Calculadora de Bhaskara')
+write ("Calculadora de raízes \n de uma equação de segundo grau")
+write ( "ax² + bx + c = 0" )
 
 #Entrada de dados
-a = st.number_input('Digite o valor de a:')
-b = st.number_input('Digite o valor de b:')
-c = st.number_input('Digite o valor de c:')
+a = text_input('Digite o valor de a:')
+b = text_input('Digite o valor de b:')
+c = text_input('Digite o valor de c:')
 
 
 
 #Processamento de dados
-if st.button('Calcular raízes'):
+if button('Calcular raízes'):
     try:
         a = float(a)
         b = float(b)
         c = float(c)
         delta = b**2 - 4*a*c
         if delta < 0:
-            st.warning('A equação não possui raízes reais.')
+            warning('A equação não possui raízes reais.')
         elif delta ==0:
-            raiz = (-b + mt.sqrt(delta)) / (2*a)
-            st.success(f'A equação possui uma raiz real: {raiz}')
+            raiz = (-b + calculo(delta))
+            success(f'A equação possui uma raiz real: {raiz}')
         else:
-            raiz1 = (-b + mt.sqrt(delta)) / (2*a)
-            raiz2 = (-b - mt.sqrt(delta)) / (2*a)
-            st.success(f'As raízes da equação são: \n Raiz 1: {raiz1} \n Raiz 2: {raiz2}')
-    except:
-        st.error("Por favor, insira valores numéricos válidos para a, b e c.")
+            raiz1 = (-b + calculo(delta))
+            raiz2 = (-b - calculo(delta))
+            success(f'As raízes da equação são: \n Raiz 1: {raiz1} \n Raiz 2: {raiz2}')
+    except ValueError:
+        error("Por favor, insira valores numéricos válidos para a, b e c.")
+    except ZeroDivisionError:
+        error("O valor de 'a' não pode ser zero em uma equação de segundo grau.")
 
