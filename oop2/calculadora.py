df3e4b99062e6b98250bdfd5344dd801fb3838b8
gucruz9 <contato.one9@gmail.com>
2025-11-14 20:40:30 -0300

    sexta2

--- oop2/calculadora.py
+++ oop2/calculadora.py
@@ -1,7 +1,9 @@
 class calculadora1:
     PI =  3.14
-
-    def circunferencia(self, raio) -> float:
-        return 2 * self.PI * raio
-    def area (self, raio) -> float:
-        return self.PI * raio ** 2
+    #Metodos da classe
+    @staticmethod
+    def circunferencia(raio) -> float:
+        return 2 * calculadora1.PI * raio
+    @staticmethod
+    def area (raio) -> float:
+        return calculadora1.PI * raio ** 2
--- oop2/circulo1.py
+++ oop2/circulo1.py
@@ -1,17 +1,15 @@
-import calculadora as c
-
-#Instânciação do objeto
-circulo = c.calculadora1()
+from calculadora import calculadora1
 
 #Entrada de dados
 raio = float(input("Digite o valor do raio: "))
 
 #Processamento de dados
-circunferencia = circulo.circunferencia(raio)
-area = circulo.area(raio)
+circunferencia = calculadora1.circunferencia(raio)
+area = calculadora1.area(raio)
+6
 
 #Saída de dados
-print(f''' Circunferência: {circunferencia::.2f}
-      Area: {area:.2f}
-      PI: {circulo.PI}    
+print(f''' Circunferência: {circunferencia:.2f}
+    Area: {area:.2f}
+      PI: {calculadora1.PI}    
 ''')
